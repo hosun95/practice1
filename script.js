2e99936fc295cfdc22a1e61100a8d2901ccb036f
hosunna <hosunna1191@gmail.com>
2022-10-02 23:12:09 -0400

    update contents

---
+++ script.js
@@ -0,0 +1,32 @@
+// let stringsLongerThan= ['say', 'hello', 'in', 'the', 'morning']
+
+
+// // let stringsLongerThan = (['say', 'hello', 'in', 'the', 'morning'], 3);
+
+// for (let i = 0; i < stringsLongerThan.length; i++) {
+//     console.log(stringsLongerThan[i].length)
+// }
+
+ 
+
+/********************/
+
+let words = ['say', 'morning', 'in', 'the', 'hello']
+
+
+function longestString() {
+    let number = '3';
+    for (let i = 0; i < words.length; i++) {
+        console.log(words[i].length)
+    
+      if (words[i].length > number) {
+        // return words[i].length;
+      }
+    }
+    // return words;
+  }
+
+console.log(longestString())
+
+//   console.log(longestString())
+
